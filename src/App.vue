ffc9e09ced67163d2e1497d4808ccc15be777d56
thonpair <emmanuel@hemer.it>
2021-02-23 11:03:13 +0100

    circle arround countdown

--- src/App.vue
+++ src/App.vue
@@ -21,16 +21,47 @@
       <button id="session-decrement" v-on:click="minusSessionLen">-</button>
       <button id="session-increment" v-on:click="plusSessionLen">+</button>
       <p id="session-length">{{ sessionLen }}</p>
-      <label v-if="sessionRunning" id="timer-label">Session</label>
-      <label v-else id="timer-label">Break</label>
+      
     </div>
-    <div id="countdown">
-      <p id="time-left" :key="timeLeft">{{ timeLeft }}</p>
-    </div>
-    <div id="buttons">
+        <div id="buttons">
       <button id="start_stop" v-on:click="startStop">start / stop</button>
       <button id="reset" v-on:click="reset">reset</button>
     </div>
+    <div class="base-timer">
+      <svg
+        class="base-timer__svg"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g class="base-timer__circle">
+          <circle
+            class="base-timer__path-elapsed"
+            cx="50"
+            cy="50"
+            r="45"
+          ></circle>
+          <path
+            id="base-timer-path-remaining"
+            class="base-timer__path-remaining"
+            v-bind:class="remainingPathColor "
+            :stroke-dasharray="circleDasharray"
+            d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+          ></path>
+        </g>
+      </svg>
+              <span v-if="sessionRunning" id="timer-label" class="base-timer__title">Session</span>
+        <span v-else id="timer-label" class="base-timer__title">Break</span>
+      <span id="base-timer-label" class="base-timer__label">
+      {{
+        timeLeft
+      }}</span>
+    </div>
+
     <audio
       id="beep"
       preload="auto"
@@ -40,8 +71,27 @@
 </template>
 
 <script>
-const DEF_BREAK = 5;
-const DEF_SESSION = 25;
+const DEF_BREAK = 2;
+const DEF_SESSION = 1;
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 30;
+const ALERT_THRESHOLD = 10;
+
+const COLOR_CODES = {
+  info: {
+    color: "green",
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD,
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD,
+  },
+};
+
 export default {
   name: "App",
   data() {
@@ -51,6 +101,8 @@ export default {
       running: false,
       timeLeftSec: 600,
       sessionRunning: true,
+      remainingPathColor: COLOR_CODES.info.color,
+      circleDasharray: FULL_DASH_ARRAY
     };
   },
   computed: {
@@ -74,6 +126,8 @@ export default {
     },
     updateTimeLeft() {
       this.timeLeftSec--;
+      this.setCircleDasharray();
+      this.setRemainingPathColor(this.timeLeftSec);
       if (this.timeLeftSec < 0) {
         this.playSound();
         if (this.sessionRunning) {
@@ -113,6 +167,29 @@ export default {
       this.sessionLen = DEF_SESSION;
       this.timeLeft = this.sessionLen;
     },
+    setRemainingPathColor(timeLeft) {
+      const { alert, warning, info } = COLOR_CODES;
+      if (timeLeft <= alert.threshold) {
+        this.remainingPathColor = alert.color;
+      } else if (timeLeft <= warning.threshold) {
+        this.remainingPathColor = warning.color;
+      } else {
+        this.remainingPathColor = info.color;
+      }
+    },
+
+    calculateTimeFraction() {
+      const fullLen = this.sessionRunning ? this.sessionLen*60 : this.breakLen*60
+      const rawTimeFraction = this.timeLeftSec / fullLen;
+      console.log(rawTimeFraction)
+      return (rawTimeFraction - (1 / fullLen) * (1 - rawTimeFraction));
+    },
+
+    setCircleDasharray() {
+      this.circleDasharray = `${(
+        this.calculateTimeFraction() * FULL_DASH_ARRAY
+      ).toFixed(0)} 283`;
+    },
   },
   mounted() {
     this.timeLeft = this.sessionLen;
@@ -121,12 +198,74 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  display: grid;
+  height: 100vh;
+  place-items: center;
+}
+
+.base-timer {
+  position: relative;
+  width: 300px;
+  height: 300px;
+}
+
+.base-timer__svg {
+  transform: scaleX(-1);
+}
+
+.base-timer__circle {
+  fill: none;
+  stroke: none;
+}
+
+.base-timer__path-elapsed {
+  stroke-width: 7px;
+  stroke: grey;
+}
+
+.base-timer__path-remaining {
+  stroke-width: 7px;
+  stroke-linecap: round;
+  transform: rotate(90deg);
+  transform-origin: center;
+  transition: 1s linear all;
+  fill-rule: nonzero;
+  stroke: currentColor;
+}
+
+.base-timer__path-remaining.green {
+  color: rgb(65, 184, 131);
+}
+
+.base-timer__path-remaining.orange {
+  color: orange;
+}
+
+.base-timer__path-remaining.red {
+  color: red;
+}
+
+.base-timer__label {
+  position: absolute;
+  width: 300px;
+  height: 350px;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 48px;
+}
+
+.base-timer__title {
+  position: absolute;
+  width: 300px;
+  height: 200px;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 48px;
 }
 </style>
